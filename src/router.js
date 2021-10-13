8e6dc53c0fc9d1271ef7c047bc983f5d2d80a0e0
= <=>
2021-10-13 13:07:03 +0900

    change mode hash from history

--- src/router.js
+++ src/router.js
@@ -1,34 +1,42 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Products from './views/Products.vue'
-import Skills from './views/Skills.vue'
-import Experiences from './views/Experiences.vue'
-Vue.use(Router)
+/** @format */
+
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Products from './views/Products.vue';
+import Skills from './views/Skills.vue';
+import Experiences from './views/Experiences.vue';
+Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
+  mode: 'hash',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
-        path: '/products',
-        name: 'products',
-        component: Products
+      path: '/products',
+      name: 'products',
+      component: Products,
     },
     {
-        path: '/skills',
-        name: 'skills',
-        component: Skills
+      path: '/skills',
+      name: 'skills',
+      component: Skills,
     },
     {
       path: '/experiences',
       name: 'experiences',
-      component: Experiences
-    }
-  ]
-})
+      component: Experiences,
+    },
+    //ここまで
+    {
+      path: '*',
+      name: 'top',
+      component: Home,
+    },
+  ],
+});
